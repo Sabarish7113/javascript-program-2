79f8630aac17215f50b7545f392c8b784bc5f28a
Sabarish7113 <sabarish.user@gmail.com>
2024-07-19 16:49:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// range
+// function range()
+// {
+//     let a =11
+//     let b = []
+//     for(let i=0;i<a;i++)
+//     {
+//         b.push(i)
+//     }
+//     console.log(b)
+// }
+// range()
+
+// checking
+
+// let a = [1,2,3,4,5,6,7,8,9,10]
+// for(let i=0;i<a.length;i++)
+// {
+//     if(i%2!=0)
+//     {
+//         document.write(i)
+//     }
+// }
+
+
+//map
+// const a = [10,30,50,80,90]
+// const b=a.map(a=>a>30)
+// document.write(b)
+
+//filter
+// const a = [10,30,50,80,90]
+// const b=a.filter(a=>a>30)
+// document.write(b)
+
+
+//flattering array
+// let a =[[1,2,3],[4,5,6],[7,8,9]]
+// function flattering(a)
+// {
+//     return a.flat()
+// }
+// document.write(flattering(a))
+
+
+
+
+
+
+
+
+
+
+
+
